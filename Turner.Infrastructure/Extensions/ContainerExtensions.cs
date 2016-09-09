9b408eb964eb0a5cd32212eec3c862a8ccaa6c99
mpuneky <mpuneky@turner-industries.com>
2016-09-09 11:18:24 -0500

    feat: added ContainerExtensions

---
+++ Turner.Infrastructure/Extensions/ContainerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SimpleInjector;
+using System.Reflection;
+
+namespace Turner.Infrastructure.Extensions
+{
+    public static class ContainerExtensions
+    {
+        public static void ConfigureInfrastructure(this Container container)
+        {
+            container.Register(typeof(IValidator<>), new[] { Assembly.GetCallingAssembly() });
+        }
+    }
+}
