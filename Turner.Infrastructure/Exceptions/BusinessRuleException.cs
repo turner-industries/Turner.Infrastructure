5a5314af261721e8daf6e54f95b1df9799df9088
Brandon Cornett <bcornett@turner-industries.com>
2016-09-12 09:34:53 -0500

    feat: add business rule violations

---
+++ Turner.Infrastructure/Exceptions/BusinessRuleException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Turner.Infrastructure.Exceptions
+{
+    [Serializable]
+    public class BusinessRuleException : Exception
+    {
+        public ICollection<BusinessRuleViolation> Violations { get; protected set; }
+
+        public BusinessRuleException(ICollection<BusinessRuleViolation> violations)
+        {
+            Violations = violations;
+        }
+    
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+
+            info.AddValue("Violations", Violations, typeof(ICollection));
+        }
+    }
+}
---
+++ Turner.Infrastructure/Exceptions/BusinessRuleViolation.cs
@@ -0,0 +1,15 @@
+﻿namespace Turner.Infrastructure.Exceptions
+{
+    public class BusinessRuleViolation
+    {
+        public string Violation { private set; get; }
+
+        public string PropertyName { private set; get; }
+
+        public BusinessRuleViolation(string violation, string propertyName = "")
+        {
+            Violation = violation;
+            PropertyName = propertyName;
+        }
+    }
+}
